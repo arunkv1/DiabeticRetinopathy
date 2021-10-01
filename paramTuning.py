1016c77b937f8df782a82f46f37635466a313858
arunkv1 <32751223+arunkv1@users.noreply.github.com>
2021-10-01 11:13:07 -0400

    Add files via upload

---
+++ paramTuning.py
@@ -0,0 +1,109 @@
+# importing the os package to add a package to pyspark submit
+import os
+# this will add the databrick's sparkdl package to the pyspark submit function.
+# This package will allow us to use the sparkdl package, and the functions like DeepImageFeaturizer
+# to perform deep learning and image classification
+SUBMIT_ARGS = "--packages databricks:spark-deep-learning:1.0.0-spark2.3-s_2.11 pyspark-shell"
+# This will add that package to the arguments used when the spark-submit is called
+os.environ["PYSPARK_SUBMIT_ARGS"] = SUBMIT_ARGS
+# Setting up the spark session
+from pyspark.sql import SparkSession
+spark = SparkSession.builder.appName("DL image recognition").getOrCreate()
+sc = spark.sparkContext
+# importing the required pyspark librarues
+from pyspark.ml.image import ImageSchema
+from pyspark.sql import Row
+from pyspark.sql.types import IntegerType
+from pyspark.sql.functions import monotonically_increasing_id
+# path to the training images in AWS S3 bucket
+train15Img = "s3://final-project-657/resized-train-15/"
+# path to the training labels in AWS S3 bucket
+labelDf = spark.read.csv("s3://final-project-657/trainLabels15.csv")
+# This will go through the labels DF, and give column names to each row
+labels = labelDf.rdd.map(lambda r: Row(imgName=str(r._c0), label=int(r._c1)))
+# add an id to each corresponding label in the labels data frame
+labelsDf = spark.createDataFrame(labels).withColumn("id", monotonically_increasing_id())
+labelsDf = labelsDf.drop("imgName")
+print(labelsDf.show())
+# This will convert the label in the dataframe to an Integer type from a string type
+changedTypedf = labelsDf.withColumn("label", labelsDf["label"].cast(IntegerType()))
+# This will use the ImageSchema library to read in the images.
+# This readImages function will read in each image as an image struct, and we set an column with a unique id with each image
+train15Eyes = ImageSchema.readImages(train15Img).withColumn("id", monotonically_increasing_id())
+print(train15Eyes.show())
+# This will join the labels df and the image df on that id, so each label is correctly corresponded with the images
+dataDf = train15Eyes.join(changedTypedf, on="id")
+# These are the combinations of parameters that we wanted to test out for cross validation
+maxIters = [10, 20, 30]
+regParams = [0.05, 0.1, 0.2]
+elasticParams = [0.3, 0.5, 0.7]
+# opening the file to write all the results to in AWS S3
+file = open("s3://final-project-657/crossVals.txt", "w")
+file.write("---------- Cross validation Results ----------\n\n")
+# This will go through every combination of the three parameters
+count = 0
+for m in maxIters:
+    for r in regParams:
+        for e in elasticParams:
+            # writing which combination is currently being executed
+            file.write("MaxIters = " + str(m) + "\n")
+            file.write("regParam = " + str(r) + "\n")
+            file.write("elasticNetParam = " + str(e) + "\n")
+            # this will split the data into a 70% train data, and 30% test data
+            trainDf, testDf = dataDf.randomSplit([0.7, 0.3], seed=10)
+            print(trainDf.show())
+            print("Initializing featurizers and LR nd pipeline")
+            # This will import the required libraries to perform the image recognition
+            from pyspark.ml.classification import LogisticRegression
+            from pyspark.ml import Pipeline
+            from sparkdl import DeepImageFeaturizer 
+            # Using the deep image featureizer to convert the images into numerical vectors
+            # DeepImageFeaturizer is from the databricks package
+            # It will fit each image into the InceptionV3 convolutional Neural Network, and convert them into numerical vectors
+            featurizer = DeepImageFeaturizer(inputCol="image", outputCol="features", modelName="InceptionV3")
+            # This will inialize the logistic regression model with the current combination of the parameters
+            lr = LogisticRegression(maxIter=m, regParam=r, elasticNetParam=e, labelCol="label")
+            # The pipeline is initialized with both stages passed through it (which are the image featurizer and Logistic regression)
+            p = Pipeline(stages=[featurizer, lr])
+            print("fitting")
+            # This will fit the training data into the pipeline
+            p_model = p.fit(trainDf)
+            # This will predict the values of test dataframe using the pipline
+            predDf = p_model.transform(testDf)
+            # selecting only the prediction and the label from the resulting dataframe
+            predAndLabels = predDf.select("prediction", "label")
+            pl = predAndLabels.rdd.map(lambda x: (x.prediction, x.label))
+            # Saving the tuples to a text file for further analysis of results
+            pl.coalesce(1).saveAsTextFile("s3://final-project-657/Predictions/results")
+            # This will open the results file with contains all the tuples of prediction and labels
+            results = open("s3://final-project-657/Predictions/results/part-00000")
+            # initializng the variables with total count and the correct number of counts
+            totalCount = 0
+            correctCount = 0
+            # This will go through each tuple in the results file
+            for line in results:
+                # converts each line into a tuple with integer and double
+                tup = eval(line)
+                # Checks to see if the prediction and the label match
+                if int(tup[0]) == int(tup[1]):
+                    correctCount += 1
+                totalCount += 1
+            # This will get the accuracy based on the number of correct counts over the total number of predictions
+            accuracy = correctCount/totalCount
+            print("----- Results -----")
+            print("Accuracy = " + str(accuracy))
+            # Wrting the accuracy to the file
+            file.write("Accuracy = " + str(accuracy) + "\n\n")
+    count += 1
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ readme.txt
@@ -0,0 +1,83 @@
+------- Final Project README -------
+In this folder, you will see 5 things. 
+
+1) Source Code (This directory will have the source code for the project)
+   - diabeticRetinopathy.py (main code, and will output accuracy after running)
+   - paramTuning.py (code to find the the best parameters for model)
+   - crossVals.py (the cross validation results with accuracy for every combination of params)
+2) Report (This directory will have the report for our project)
+3) video_ajay-krishanavajjala_arun-krishnavajjala.txt (has link of our video)
+4) data-url.txt (link to data for our project)
+5) readme.txt (this file, which is the readme for the project)
+
+Running the Project:
+
+To run the project, all you have to do is change the path to the file name in all the python files. The current file name is on our aws s3 bucket, so just change the file name to the one on your own personal computer. Also, in order to run the data bricks package's functions, you will need to download the necessary versions of the packages. The DeepImageFeaturizer will only work with tensor flow 1.6, and keras 2.5. In addition to this, you may have to download other required pip libraries and the current versions to make the code work. We created a conda environment on our EMR cluster, and downloaded all the required packages and correct versions for it to run perfectly. Once the correct versions are installed from the list in this readme file. Then the code will run perfectly. If you are having trouble running the file, feel free to reach out to us. Our emails are at the end of this readme file. 
+
+---- The Required Pip Packages To Run The Code ----
+
+Package            Version  
+------------------ ---------
+absl-py            0.11.0   
+appnope            0.1.2    
+astor              0.8.1    
+backcall           0.2.0    
+bleach             1.5.0    
+certifi            2020.6.20
+cloudpickle        1.6.0    
+coverage           4.4.1    
+decorator          4.4.2    
+gast               0.4.0    
+grpcio             1.34.0   
+h5py               2.7.0    
+html5lib           0.9999999
+importlib-metadata 2.1.1    
+ipykernel          5.3.4    
+ipython            7.9.0    
+ipython-genutils   0.2.0    
+jedi               0.17.2   
+jupyter-client     6.1.7    
+jupyter-core       4.6.3    
+Keras              2.1.5    
+Markdown           3.2.2    
+nose               1.3.7    
+numpy              1.16.4   
+olefile            0.46     
+pandas             0.19.1   
+parameterized      0.6.1    
+parso              0.7.1    
+pexpect            4.8.0    
+pickleshare        0.7.5    
+Pillow             4.1.1    
+pip                10.0.1   
+prompt-toolkit     2.0.10   
+protobuf           3.14.0   
+ptyprocess         0.6.0    
+py4j               0.10.6   
+Pygments           2.2.0    
+pyspark            2.3.0    
+python-dateutil    2.8.1    
+pytz               2020.4   
+PyYAML             5.3.1    
+pyzmq              20.0.0   
+scipy              1.4.1    
+setuptools         40.2.0   
+six                1.10.0   
+sparkdl            0.2.2    
+spyder-kernels     1.9.1    
+tensorboard        1.6.0    
+tensorflow         1.6.0    
+tensorframes       0.2.9    
+termcolor          1.1.0    
+tornado            6.1      
+traitlets          4.3.3    
+wcwidth            0.2.5    
+Werkzeug           1.0.1    
+wheel              0.36.0   
+wrapt              1.12.1   
+wurlitzer          2.0.1    
+zipp               1.2.0 
+
+
+Ajay e-mail: akrish@gmu.edu
+Arun e-mail: akrishn@gmu.edu
